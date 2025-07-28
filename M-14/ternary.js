dffa72fedf024e9057989eb97a667444b70ff90d
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-07-28 14:27:03 +0600

    ternary

---
+++ M-14/ternary.js
@@ -0,0 +1,17 @@
+// Ternary --> three parts
+//   ?   :
+
+//   condition ? do something when true : do something when false
+
+const age1 =20;
+if(age1>=18)
+{
+    console.log("you can vote");
+}
+
+else{
+    console.log("you can not vote");
+}
+const age2 = 9;
+
+age2>=18 ? console.log(' can vote') : console.log("cannot vote")

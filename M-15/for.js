ec1b44e4e9aae4d866d2fd971cf570278345bfb3
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-08-03 22:05:55 +0600

    for loop

---
+++ M-15/for.js
@@ -0,0 +1,19 @@
+//while loop
+let num = 0; //loop variable
+
+while (num < 5) { //loop condition
+  console.log(num); //loop statement
+  // num+=1;
+  num++; //loop variable change
+}
+
+
+// for(first part-variable init; second part--condition; third part==variable change){
+//     loop statement
+// }
+
+for(let num = 0 ; num<5; num++)
+{
+    console.log(num);
+    
+}

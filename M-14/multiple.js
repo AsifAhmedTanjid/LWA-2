01761a52301fe413c3dd0a45a0aff5a3262a2201
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-08-03 21:30:41 +0600

    m15 starting with recap

--- M-14/multiple.js
+++ M-14/multiple.js
@@ -1,10 +1,15 @@
-const salary =25000;
-const isBCS =true;
+// const salary =25000;
+// const isBCS =true;
 
-if(salary>20000 && isBCS )
-{
-    console.log("good");
-}
-else{
-    console.log("look forward");
+// if(salary>20000 && isBCS )
+// {
+//     console.log("good");
+// }
+// else{
+//     console.log("look forward");
+// }
+
+
+for (let i = 0; i < 5; i+=3) {
+ console.log(i);
 }
---
+++ M-15/recap.js
@@ -0,0 +1,16 @@
+//const- value will not change usually
+//let - value can change
+// var - we should not use it
+
+const amount=200;
+
+if(amount>100){
+    //do something
+}
+else{
+    //do something else
+}
+
+//array
+
+const numbers = [12, 32,54,65];

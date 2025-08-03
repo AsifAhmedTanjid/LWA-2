66d0f6ffbbf12a82bb6e3634b2c9bf496e994d06
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-08-03 23:27:10 +0600

    includes,indexof

---
+++ M-15.5/includes.js
@@ -0,0 +1,3 @@
+const friends =['abul','bulbul','habul','asif'];
+console.log(friends.includes('asif'));
+console.log(friends.includes('Asif'));
---
+++ M-15.5/indexOf.js
@@ -0,0 +1,4 @@
+const friends = ["abul", "bulbul", "habul"];
+
+console.log(friends.indexOf("bulbul"));
+console.log(friends.indexOf("baaaaaabul")); //if there is not then returns -1

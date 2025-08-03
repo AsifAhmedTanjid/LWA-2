7444bf272fcc658d6c8bab6fac9e30d1b73f6079
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-08-03 23:10:26 +0600

    array indices and set update by index number

---
+++ M-15.5/index.js
@@ -0,0 +1,11 @@
+const numbers =[1, 4 ,2 ,5 ,23 ,43 ,22 ,44 ,222 ,5523 ,113 , 13234];
+//get element value by index
+console.log(numbers);
+console.log(numbers[5]);
+const fourth= numbers[3];
+console.log(fourth);
+
+//set or update element value by index
+
+numbers[1] = 999;
+console.log(numbers);

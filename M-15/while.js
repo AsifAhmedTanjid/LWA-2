519da695c2e89fcbd22ed2d2fc1a8047e28a184e
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-08-03 21:48:16 +0600

    while loop intro

---
+++ M-15/while.js
@@ -0,0 +1,19 @@
+const number = 7;
+if(number<1){
+    console.log("less");
+    
+
+}
+console.log('something new is cooking');
+
+
+//while loop
+let num =0; //loop variable
+
+while (num<5) {
+    console.log('looping',num);
+    // num+=1;
+    num++;
+    
+    
+}

84b7e0db84102c4fe9b9850245fcc31242d7e864
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-08-03 21:38:27 +0600

    for-of loop intro

---
+++ M-15/for-of.js
@@ -0,0 +1,17 @@
+const numbers = [12, 32,54,65,21,72];
+console.log(numbers);
+
+for(const num of numbers){
+    console.log(num);
+}
+
+const fruits = ['apple','mango','banana','jackfruit','watermelon']
+
+// for(){
+
+// }
+
+for(const fruit of fruits){
+    console.log(fruit);
+    
+}

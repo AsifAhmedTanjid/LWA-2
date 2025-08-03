b2871c21b8629b445077d33247d6ef8e6f6374a1
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-08-03 22:39:09 +0600

    break-continue statement

---
+++ M-15/continue.js
@@ -0,0 +1,10 @@
+// continue -- skip  REST OF THE CODE FOR this iteration
+// break -- I AM DONE WITH THE LOOP . LOOP ENDS
+
+for(let i=1;i<10;i++)
+{
+    if(i%2===1){
+        continue;
+    }
+    console.log(i);
+}

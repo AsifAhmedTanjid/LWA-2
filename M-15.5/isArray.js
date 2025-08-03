8286132938d81c031cd18c2c264c5a0676700d63
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-08-03 23:32:03 +0600

    is Array

---
+++ M-15.5/isArray.js
@@ -0,0 +1,6 @@
+const friends =['abul','bulbul','habul'];
+const nums =[];
+const food ='ros o gol l a';
+console.log(Array.isArray(friends));
+console.log(Array.isArray(nums));
+console.log(Array.isArray(food));

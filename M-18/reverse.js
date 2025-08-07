81cb1e0dd5871216f559e9d9b97b2455a0363114
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-08-07 10:59:46 +0600

    array reverse js // more way

--- M-18/reverse.js
+++ M-18/reverse.js
@@ -1,6 +1,7 @@
-const numbers= [1,2,3,4,5,6]
-const reversed1 =[];
-const reversed2 =[];
+const numbers = [1, 2, 3, 4, 5, 6];
+const reversed1 = [];
+const reversed2 = [];
+const reversed3 = [];
 //one way
 for (let i = 0; i < numbers.length; i++) {
   console.log(numbers[i]);
@@ -11,8 +12,20 @@ console.log(reversed1);
 
 //another way
 
-for (let i = numbers.length-1; i >=0; i--) {
-//   console.log(numbers[i]);
+for (let i = numbers.length - 1; i >= 0; i--) {
+  //   console.log(numbers[i]);
   reversed2.push(numbers[i]);
 }
 console.log(reversed2);
+
+//using for of
+
+for (let num of numbers) {
+  //   console.log(numbers[i]);
+  reversed3.unshift(num);
+}
+console.log(reversed3);
+
+//using reverse function
+
+console.log(numbers.reverse());

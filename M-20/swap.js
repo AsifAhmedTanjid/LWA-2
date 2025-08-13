ab0a807a08ec322c42c08403aef0257f129dc85c
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-08-13 19:25:23 +0600

    swap variable using temporary variable

---
+++ M-20/swap.js
@@ -0,0 +1,29 @@
+
+// using temporary variable : take extra spaces
+let a =5;
+let b =7;
+
+
+console.log(a,b);
+
+const temp = a;
+a=b;
+b=temp;
+
+console.log(a,b);
+
+//---------------------------
+//destructing
+let x=8;
+let y=9;
+
+console.log(x,y);
+
+[x,y]=[y,x]
+
+console.log(x,y);
+
+//--------------------
+
+
+

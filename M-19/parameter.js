6f6fc1bac25144fb30fd6d4c7f4d1257af8abe86
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-08-13 16:05:40 +0600

    funtions parameter

---
+++ M-19/parameter.js
@@ -0,0 +1,18 @@
+function square(number)
+{
+    console.log('value of the number parameter', number);
+    const borgo = number*number;
+    console.log('square of the given number is ',borgo);
+    
+    
+}
+square(15);
+square(10);
+
+function add(num1,num2){
+    const sum = num1+num2;
+    console.log('the sum is' , sum);
+    
+}
+
+add(10,15);

002c69b8445c0ae2f6c76a4c2eaf14ffd59279a0
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-08-13 15:53:59 +0600

    intro to function

---
+++ M-19/function.js
@@ -0,0 +1,15 @@
+const number = 40;
+//funtion declaration
+function FanOff(){
+    console.log('turn of the fan switch');
+    
+}
+
+//calling the function
+FanOff();
+
+function brushTeeth(){
+    console.log('Brush teeth');
+    
+}
+brushTeeth();

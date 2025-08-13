6c3f34eca06a65e5cd16bbcda9cda55a35d95c21
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-08-13 19:12:22 +0600

    some math functions

---
+++ M-20/math.js
@@ -0,0 +1,27 @@
+// const customsumm ={
+//     sum: function sum(num1,num2){
+//         return num1+num2;
+//     }
+// }
+
+// const sum = customsumm.sum(2,4);
+// console.log(sum);
+
+
+const min = Math.min(34,4433,2132,1,33,12,434,4);
+console.log(min);
+const max = Math.max(34,4433,2132,1,33,12,434,4);
+console.log(max);
+
+console.log(Math.PI);
+console.log(Math.abs(1-10));
+console.log(Math.round(1.5));
+console.log(Math.round(1.49));
+console.log(Math.ceil(2.49));
+console.log(Math.floor(2.49));
+console.log(Math.random());
+console.log(Math.random()*10);
+
+
+
+

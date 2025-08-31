190b941f9783cf16dd4b7bce99241884ed2807c2
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-09-01 03:48:04 +0600

    validation

---
+++ M-28.5/validation.js
@@ -0,0 +1,20 @@
+const number=23;
+if(typeof number === 'number'){
+    console.log('number');
+    
+}
+else
+{
+    console.log('not a number');
+    
+}
+
+const numbers =[1,1,1,1,1,1];
+
+console.log(typeof(numbers),Array.isArray(numbers));
+const student ={name:'asif',
+    roll:23
+}
+
+console.log(typeof student);
+

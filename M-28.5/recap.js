56e4dd3bd1f2c7a87dce4037700a3dbdbb5e1bb5
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-09-01 03:38:23 +0600

    recap

---
+++ M-28.5/recap.js
@@ -0,0 +1,24 @@
+/**
+ * 1. how to use js with html and css
+ * 
+ * 2. get element/s from dom
+ *      2-1. getElementById
+ *      2-2. getElementByClassName
+ *      2-3.getElementByTagName
+ *      2-4. querySelector
+ *      2-5. querySelectorAll
+ * 
+ * 3.Dynamically add element to the dom document.createElement
+ *  3-1.set value to the interhtml
+ * 
+ * 4. get and set value from/to dom element
+ *      4.1 innerText
+ *      4.2.use value (input tag or textarea)
+ * 
+ * 5. add event listener (eventhandler)
+ *  
+ * 
+ * 6. use function to reduce duplicate code
+ * 
+ * 7. event bubble and delegate
+ */

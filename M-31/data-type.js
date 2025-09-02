fd1b94063a8bdbddb8f70c3940cfc97ae13748e5
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-09-02 10:44:37 +0600

    Primitive and non-primitive data type

---
+++ M-31/data-type.js
@@ -0,0 +1,27 @@
+//javaScript is dynamic typed programming language
+
+let data =4; //number
+data ='hello'; //string
+data=false; //boolean
+data={name:'hello'} //object type
+data=[1,2,3] //object type, check with Array.isArray()
+data=undefined //undefine
+data=null; //object type
+console.log(typeof data);
+
+
+//primitive datatype : number ,string,boolean ,undefine,null
+
+const num=10;
+const name='js';
+const isPoor=false;
+let isKnown= undefined; //should not set the value undefine
+let isLocal;  //will also be undefine as we didnt initialize
+const merit=null;
+
+//non-primitive
+const student ={
+    name:'js',
+    mark:0
+};
+const numbers=[1,2,3,4];

fa25b43241c54feb8e9ba64e11467899bb10046b
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-09-03 11:37:03 +0600

    Display Post Card & Style using CSS

--- M-32/posts.js
+++ M-32/posts.js
@@ -1,41 +1,62 @@
-const loadPost=()=>{
-    const url='https://jsonplaceholder.typicode.com/posts';
-    fetch(url)
-    .then(res=>res.json())
-    .then(data=>{
-        // console.log(data);
-        displayPost(data);
-        
-
-    })
-}
-//array of object
-const displayPost=(posts)=>{
-    console.log(posts);
-    // for(i=0;i<posts.length;i++){
-    //     console.log(posts[i]);
-        
-    // }
-    // for(let post of posts){
-    //     console.log(post);
-        
-    // }
-
-    //1. get the container
-    const postContainer=document.getElementById('post-container');
-    // console.log(postContainer);
-    
-    
-    posts.forEach(post => {
-        // console.log(post.title);
-        // 2.create html element 
-        const li=document.createElement('li');
-        li.innerText=post.title;
-        // console.log(li);
-
-        //3. add li into container
-        postContainer.appendChild(li);
-        
+const loadPost = () => {
+  const url = "https://jsonplaceholder.typicode.com/posts";
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data);
+      displayPost(data);
     });
-    
-}
+};
+// //array of object
+// const displayPost=(posts)=>{
+//     console.log(posts);
+//     // for(i=0;i<posts.length;i++){
+//     //     console.log(posts[i]);
+
+//     // }
+//     // for(let post of posts){
+//     //     console.log(post);
+
+//     // }
+
+//     //1. get the container and empty the container
+//     const postContainer=document.getElementById('post-container');
+//     postContainer.innerHTML='';
+//     // console.log(postContainer);
+
+//     posts.forEach(post => {
+//         // console.log(post.title);
+//         // 2.create html element
+//         const li=document.createElement('li');
+//         li.innerText=post.title;
+//         // console.log(li);
+
+//         //3. add li into container
+//         postContainer.appendChild(li);
+
+//     });
+
+// }
+
+//array of object
+const displayPost = (posts) => {
+  //1. get the container and empty the container
+  const postContainer = document.getElementById("post-container");
+  postContainer.innerHTML = "";
+  // console.log(postContainer);
+
+  posts.forEach((post) => {
+    // 2.create html element
+    const postCard = document.createElement("div");
+    postCard.innerHTML = `        <div class="post-card">
+            <h2>${post.title}</h2>
+            <p>${post.body}</p>
+        </div>`
+   
+
+    //3. add li into container
+    postContainer.appendChild(postCard);
+  });
+};
+
+loadPost();

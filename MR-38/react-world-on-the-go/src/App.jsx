ef22a36b7c6bcce0ed14431dd7a6864dd88b0c47
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-09-20 23:26:32 +0600

    Load and display countries with related information

---
+++ MR-38/react-world-on-the-go/src/App.jsx
@@ -0,0 +1,29 @@
+
+import { Suspense } from 'react'
+import './App.css'
+import Countries from './components/Countries/Countries'
+
+
+const countriesPromise = fetch("https://openapi.programming-hero.com/api/all")
+.then(res=>res.json())
+
+function App() {
+ 
+
+
+
+
+  return (
+    <>
+      
+      <h1>React World</h1>
+
+     <Suspense fallback={<p>Loading...</p>}>
+      <Countries countriesPromise={countriesPromise}></Countries>
+     </Suspense>
+ 
+    </>
+  )
+}
+
+export default App

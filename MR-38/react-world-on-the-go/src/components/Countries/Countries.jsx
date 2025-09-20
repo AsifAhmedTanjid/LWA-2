9b9bcf281638f93bf4443e7e6ca42d5ea8ef51dd
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-09-20 23:39:18 +0600

    Use style in react and 3 columns layout and conditional text

--- MR-38/react-world-on-the-go/src/components/Countries/Countries.jsx
+++ MR-38/react-world-on-the-go/src/components/Countries/Countries.jsx
@@ -1,17 +1,20 @@
-import React, { use } from 'react';
-import Country from '../Country/Country';
+import React, { use } from "react";
+import Country from "../Country/Country";
+import "./Countries.css";
 
-const Countries = ({countriesPromise}) => {
-    const countriesData=use(countriesPromise);
-    const countries=countriesData.countries;
-    return (
-        <div>
-            <h1>in the countries:{countries.length}</h1>
-            {
-                countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
-            }
-        </div>
-    );
+const Countries = ({ countriesPromise }) => {
+  const countriesData = use(countriesPromise);
+  const countries = countriesData.countries;
+  return (
+    <div>
+      <h1>in the countries:{countries.length}</h1>
+      <div className="countries">
+        {countries.map((country) => (
+          <Country key={country.cca3.cca3} country={country}></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
--- MR-38/react-world-on-the-go/src/components/Country/Country.jsx
+++ MR-38/react-world-on-the-go/src/components/Country/Country.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import './Country.css'
 
 const Country = ({country}) => {
+
+    const handleVisited=()=>{
+            console.log("button clicked");
+            
+    }
     return (
-        <div>
+        <div className='country'>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h2>Name: {country.name.common}</h2>
             <p>Population: {country.population.population}</p>
+            <p>Area: {country.area.area} {country.area.area>300000?"Big country":"Small Country"}</p>
+
+            <button onClick={handleVisited}>Not Visited</button>
         </div>
     );
 };

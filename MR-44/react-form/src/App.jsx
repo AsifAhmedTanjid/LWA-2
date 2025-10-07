9b09d1dd2d0b260d126710292f0ed2bd76127373
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-10-07 17:03:42 +0600

    react form

---
+++ MR-44/react-form/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import FormAction from './components/FormAction/FormAction'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Explore react form</h1>
+      <FormAction></FormAction>
+    </>
+  )
+}
+
+export default App
---
+++ MR-44/react-form/src/components/FormAction/FormAction.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const FormAction = () => {
+const handleFormAction =
+
+    return (
+        <div>
+            <form action="">
+                <input type="text" placeholder='name' />
+                <input type="email" placeholder='email' />
+            </form>
+        </div>
+    );
+};
+
+export default FormAction;

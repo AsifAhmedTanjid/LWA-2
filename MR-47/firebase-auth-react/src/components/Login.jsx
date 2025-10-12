0aef732bc315da91b497dd28c7bdceff0153c448
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-10-12 22:56:23 +0600

    Enable Github sign-in and Create GitHub application

--- MR-47/firebase-auth-react/src/components/Login.jsx
+++ MR-47/firebase-auth-react/src/components/Login.jsx
@@ -1,49 +1,68 @@
-import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
-import React, { useState } from 'react';
-import { auth } from '../firebase/firebase.init';
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import React, { useState } from "react";
+import { auth } from "../firebase/firebase.init";
 
 const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
 const Login = () => {
-    const [user,setUser]=useState(null);
-    const handleGoogleSignIn = ()=>{
-        console.log('google btn clicked');
-        signInWithPopup(auth,googleProvider)
-        .then(result=>{
-            console.log(result.user);
-            setUser(result.user);
-            
-        })
-        .catch(error=>{
-            console.log(error);
-            
-        })
-        
-    }
-    const handleSignOut =()=>{
-        signOut(auth).then(()=>{
-            console.log('singout done');
-            setUser(null)
-            
-        }).catch (error =>{
-            console.log(error);
-            
-        })
-    }
-    return (
+  const [user, setUser] = useState(null);
+  const handleGoogleSignIn = () => {
+    console.log("google btn clicked");
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        console.log(result.user);
+        setUser(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, githubProvider)
+      .then((result) => {
+        console.log(result.user);
+        setUser(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        console.log("signout done");
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div>
+      <h1>Please Login</h1>
+
+      {user ? (
+        <button onClick={handleSignOut}>Sign Out</button>
+      ) : (
+        <>
+          <button onClick={handleGoogleSignIn}>SignIn WIth Google</button>
+          <button onClick={handleGithubSignIn}>SignIn WIth Github</button>
+        </>
+      )}
+      {user && (
         <div>
-            <h1>Please Login</h1>
-            
-            
-            {
-                user ?<button onClick={handleSignOut}>Sign Out</button>:<button onClick={handleGoogleSignIn}>SignIn WIth Google</button>
-            }
-          {user &&   <div>
-                <h3>{user.displayName}</h3>
-                <h5>{user.email}</h5>
-                <img src={user.photoURL} alt="" />
-            </div>}
+          <h3>{user.displayName}</h3>
+          <h5>{user.email}</h5>
+          <img src={user.photoURL} alt="" />
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Login;

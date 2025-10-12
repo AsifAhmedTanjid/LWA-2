e2db2e537de460da239e3e0ef905badfeb857950
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-10-12 22:00:23 +0600

    Created Firebase Project and Initialize Firebase auth

---
+++ MR-47/firebase-auth-react/src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBPTBTjUzHMGII-cAFhN9ShCMVjFFVF_GY",
+  authDomain: "simple-firebase-auth-58a58.firebaseapp.com",
+  projectId: "simple-firebase-auth-58a58",
+  storageBucket: "simple-firebase-auth-58a58.firebasestorage.app",
+  messagingSenderId: "34249457335",
+  appId: "1:34249457335:web:8400e86d7e1c8dd2d65732"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

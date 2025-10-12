16a4d7813e35d03475478c4a7c91129ec27b907e
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-10-12 22:15:03 +0600

    Set Sign in with Google and Open Google login popup

--- MR-47/firebase-auth-react/src/components/Login.jsx
+++ MR-47/firebase-auth-react/src/components/Login.jsx
@@ -1,9 +1,26 @@
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import React from 'react';
+import { auth } from '../firebase/firebase.init';
 
+const googleProvider = new GoogleAuthProvider();
 const Login = () => {
+    const handleGoogleSignIn = ()=>{
+        console.log('google btn clicked');
+        signInWithPopup(auth,googleProvider)
+        .then(result=>{
+            console.log(result.user);
+            
+        })
+        .catch(error=>{
+            console.log(error);
+            
+        })
+        
+    }
     return (
         <div>
             <h1>Please Login</h1>
+            <button onClick={handleGoogleSignIn}>SignIn WIth Google</button>
         </div>
     );
 };

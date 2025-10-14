47295a135c102504546a3e967a4666f8b3f843d4
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-10-14 22:31:45 +0600

    toggle Show password done

--- MR-48/email-password-auth/src/components/Register.jsx
+++ MR-48/email-password-auth/src/components/Register.jsx
@@ -1,69 +1,80 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import { auth } from "../firebase/firebase.init";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const Register = () => {
-  const [success,setSuccess]=useState(false)
-  const [error,setError]=useState("")
-    const handleRegister=(e)=>{
-        e.preventDefault();
-        const email=e.target.email.value;
-        const password=e.target.password.value;
-        console.log('CLICKS',email,password);
-        // reset status : success or error
-        setError('');
-        setSuccess(false)
-        const passwordPattern=/^(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{6,}$/;
-        if(!passwordPattern.test(password)){
-          setError("Password must be at least 6 characters long, include at least one uppercase letter, one lowercase letter, and one special character.")
-          return;
-        }
-        createUserWithEmailAndPassword(auth,email,password)
-        .then(result=>{
-          
-          console.log('after creation of new user',result.user);
-          setSuccess(true);
-          e.target.reset();
-          
-        })
-        .catch(error=>{
-          console.log('error happened',error.message);
-          setError(error.message);
-          
-        })
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState("");
+  const [showPassword,setShowPassword]=useState(false);
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log("CLICKS", email, password);
+    // reset status : success or error
+    setError("");
+    setSuccess(false);
+    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{6,}$/;
+    if (!passwordPattern.test(password)) {
+      setError(
+        "Password must be at least 6 characters long, include at least one uppercase letter, one lowercase letter, and one special character."
+      );
+      return;
     }
-    
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log("after creation of new user", result.user);
+        setSuccess(true);
+        e.target.reset();
+      })
+      .catch((error) => {
+        console.log("error happened", error.message);
+        setError(error.message);
+      });
+  };
+  const handleTogglePassword=(e)=>{
+e.preventDefault();
+setShowPassword(!showPassword)
+
+  }
+
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
           <h1 className="text-5xl font-bold">Register now!</h1>
-          
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <div className="card-body">
             <form onSubmit={handleRegister}>
               <fieldset className="fieldset">
                 <label className="label">Email</label>
-                <input name="email" type="email" className="input" placeholder="Email" />
-                <label className="label">Password</label>
                 <input
-                  type="password"
+                  name="email"
+                  type="email"
                   className="input"
-                  placeholder="Password"
-                  name="password"
+                  placeholder="Email"
                 />
+                <label className="label">Password</label>
+                <div className="relative">
+                  <input
+                    type={showPassword ? "text":"password"}
+                    className="input"
+                    placeholder="Password"
+                    name="password"
+                  />
+                  <button className="btn btn-xs absolute top-2 right-5" onClick={handleTogglePassword}>{showPassword?<FaEyeSlash></FaEyeSlash>:<FaEye></FaEye>}</button>
+                </div>
                 <div>
                   <a className="link link-hover">Forgot password?</a>
                 </div>
                 <button className="btn btn-neutral mt-4">Register</button>
               </fieldset>
-              {
-                success && <p className="text-green-500">Acoount Created Successfully</p>
-              }
-              {
-                error && <p className="text-red-500">{error}</p>
-              }
+              {success && (
+                <p className="text-green-500">Acoount Created Successfully</p>
+              )}
+              {error && <p className="text-red-500">{error}</p>}
             </form>
           </div>
         </div>

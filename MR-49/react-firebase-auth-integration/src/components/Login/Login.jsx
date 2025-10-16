7eed77e3f294818547881a789f80ca060adda88a
AsifAhmedTanjid <asifahmedtanjid@gmail.com>
2025-10-16 12:28:03 +0600

    register and login form

--- MR-49/react-firebase-auth-integration/src/components/Login/Login.jsx
+++ MR-49/react-firebase-auth-integration/src/components/Login/Login.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>Login</h1>
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
         </div>
-    );
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="card-body">
+            <form>
+              <fieldset className="fieldset">
+                {/* email field */}
+                <label className="label">Email</label>
+                <input
+                  name="email"
+                  type="email"
+                  className="input"
+                  placeholder="Email"
+                />
+                {/* password field */}
+                <label className="label">Password</label>
+                <input
+                  type="password"
+                  className="input"
+                  placeholder="Password"
+                  name="password"
+                />
+                <div>
+                  <p>
+                    New to the site?{" "}
+                    <Link to="/register" className="link link-hover">
+                      Register
+                    </Link>{" "}
+                  </p>
+                </div>
+                <button className="btn btn-neutral mt-4">Login</button>
+              </fieldset>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- MR-49/react-firebase-auth-integration/src/components/Register/Register.jsx
+++ MR-49/react-firebase-auth-integration/src/components/Register/Register.jsx
@@ -1,6 +1,24 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import React from "react";
+import { Link } from "react-router";
+import { auth } from "../../firebase/firebase.init";
 
 const Register = () => {
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const name = e.target.name.value;
+    console.log(name, email, password);
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -9,16 +27,43 @@ const Register = () => {
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <div className="card-body">
-            <fieldset className="fieldset">
-              <label className="label">Email</label>
-              <input type="email" className="input" placeholder="Email" />
-              <label className="label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
-              <div>
-                <a className="link link-hover">Forgot password?</a>
-              </div>
-              <button className="btn btn-neutral mt-4">Login</button>
-            </fieldset>
+            <form onSubmit={handleRegister}>
+              <fieldset className="fieldset">
+                {/* name field */}
+                <label className="label">Name</label>
+                <input
+                  name="name"
+                  type="text"
+                  className="input"
+                  placeholder="Name"
+                />
+                {/* email field */}
+                <label className="label">Email</label>
+                <input
+                  name="email"
+                  type="email"
+                  className="input"
+                  placeholder="Email"
+                />
+                {/* password field */}
+                <label className="label">Password</label>
+                <input
+                  type="password"
+                  className="input"
+                  placeholder="Password"
+                  name="password"
+                />
+                <div>
+                  <p>
+                    Already have an account?{" "}
+                    <Link to="/login" className="link link-hover">
+                      Login
+                    </Link>{" "}
+                  </p>
+                </div>
+                <button className="btn btn-neutral mt-4">Register</button>
+              </fieldset>
+            </form>
           </div>
         </div>
       </div>

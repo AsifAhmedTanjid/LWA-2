8e1cc67d1a60d19cf5bf9191a831d0d2aee16803
AsifAhmedTanjid <asifahmedtanjid@gmail.com>
2025-10-29 11:47:57 +0600

    database connection with express server

---
+++ MREM 54/simple-crud-server/index.js
@@ -0,0 +1,64 @@
+const express = require("express")
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app =express();
+const cors = require('cors');
+const port =process.env.PORT || 3000;
+
+const uri = "mongodb+srv://asifDBUser:JGYAQg9j3Q4wAEhM@cluster0.clghzkh.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+// /asifDBUser
+//JGYAQg9j3Q4wAEhM
+
+
+app.get("/",(req,res)=>{
+    res.send("server is running")
+})
+
+
+async function run(){
+    try {
+        await client.connect();
+        await client.db("admin").command({ping:1})
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        
+    } 
+    finally{
+        //awat client.clos e()
+    }
+}
+run().catch(console.dir)
+
+
+app.listen(port,()=>{
+    console.log(`server running on ${port}`);
+    
+})
+
+
+// try {
+    
+// } catch (error) {
+    
+// }
+// finally{
+
+// }
+
+// async function run(){
+//     // await
+// }
+// run().catch(console.dir)
